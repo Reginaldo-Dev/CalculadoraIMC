56e7e97b754b180569d3d0f091e29a4b77b58407
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-08-07 06:53:33 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-/* Manipulando dados de Formulário*/
-
-
 let formulario = document.querySelector('form')
 
-let cxNome = document.querySelector('#nome')
-let cxIdade = document.querySelector('#idade')
-let cxPeso = document.querySelector('#peso')
+let cxNome   = document.querySelector('#nome')
+let cxIdade  = document.querySelector('#idade')
+let cxPeso   = document.querySelector('#peso')
 let cxAltura = document.querySelector('#altura')
-let cxImc = document.querySelector('#resultadoImc')
+let cxImc    = document.querySelector('#resultadoImc')
 
 let aviso = document.querySelector('#aviso')
-let dados = document.querySelector('.pessoa')
+let dados = document.querySelectorAll('.pessoa')
 
 let btnEnviar = document.querySelector('#btnEnviar')
 let btnLimpar = document.querySelector('#btnLimpar')
 
-/*-- PARA PEGAR OS DADOS QUE ESTÃO DENTRO DAS CAIXAS 
-USAREI A PROPRIEDADE .VALUE, MAS ANTES DETERMINE UM
-EVENTO COMO REFERêNCIA PARA PEGAR OS DADOS --*/
+/* PARA PEGAR OS DADOS QUE ESTAO DENTRO DAS
+CAIXAS USE A PROPRIEDADE .value
+MAS, ANTES DETERMINE UM EVENTO COMO REFERENCIA PARA PEGAR OS DADOS */
 
 /* Adicionei escutador para o btnEnviar */
 // Uma funcao anonima para pegar os valores
 // calcular o imc
-
-btnEnviar.addEventListener(' click', function(e){
-    // pegar os valores de cada inptut
-    let nome = cxNome.value  
-    let idade = cxIdade.value
-    let peso = cxPeso.value
+btnEnviar.addEventListener('click', function (e) {
+    // pegar os values de cada input
+    let nome   = cxNome.value
+    let idade  = cxIdade.value
+    let peso   = cxPeso.value
     let altura = cxAltura.value
-    let imc = (peso / (altura * altura)).toFixed(1)
+    let imc    = (peso / (altura * altura)).toFixed(1)
 
     console.log(nome)
     console.log(idade)
@@ -51,33 +47,36 @@ btnEnviar.addEventListener(' click', function(e){
         'IMC ' + imc + ' ' +
         sit
     ) */
-
-    // Criar objeto pessoa
+    
+    // criar objeto pessoa
     let pessoa = {
-        nome : nome,
-        idade: idade,
-        peso : peso,
-        altura: altura,
-        imc  : imc,
-        sit  : sit,
-    }    
+        nome   : nome,
+        idade  : idade,
+        peso   : peso,
+        altura : altura,
+        imc    : imc,
+        sit    : sit,
+    }
+
     console.log(pessoa)
     dados[1].textContent = pessoa.nome
-    dados[2].textContent = pessoa.idade + "anos"
-    dados[3].textContent = pessoa.peso + "kg"
-    dados[4].textContent = pessoa.altura + "m"
+    dados[2].textContent = pessoa.idade + " anos"
+    dados[3].textContent = pessoa.peso + " Kg"
+    dados[4].textContent = pessoa.altura + " m"
     dados[5].textContent = pessoa.imc + " " + pessoa.sit
 
     // previnir o comportamento padrao
-    e.preventDefault
+    e.preventDefault()
 })
 
 function situacaoDoPeso(imc) {
     let situacao = ''
     if (imc < 18.5) {
-        situacao = 'Baixo do peso'
-    } else if (imc <= 18.5 && imc <= 29.9) {
-        situacao = 'Peso Normal'
+        situacao = 'Baixo peso'	
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        situacao = 'Peso normal'
+    } else if (imc >= 25 && imc <= 29.9) {
+        situacao = 'Sobrepeso'
     } else if (imc >= 30 && imc <= 34.9) {
         situacao = 'Obesidade I'
     } else if (imc >= 35 && imc <= 39.9) {
@@ -89,8 +88,3 @@ function situacaoDoPeso(imc) {
     }
     return situacao
 }
-
-
-
-
-
